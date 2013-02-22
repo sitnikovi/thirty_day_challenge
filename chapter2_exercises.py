a8b51f760258b00886d0c34daf32fbe01a626be2
Ilya Sitnikov <sitnikovi@hotmail.com>
2013-02-22 08:35:45 -0500

    Exercise 2.4

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,28 @@
 # Exercises for chapter 2: Problems 2.1, 2.2, 2.3, and 2.4 in Think Python
 
+# 2.4
+
+1.	width/2
+
+    8 / integer
+
+
+2.	width/2.0
+    
+    8.5 / float
+
+3.	height/3
+
+    4.0 / float
+
+4.	1 + 2 * 5
+
+    11 / integer
+
+5.	Delimiter * 5
+
+    ..... / string
+
+# to find out the type of an expression we use type(expression)
+
 # Name
